c3a05ede8f0d5f3e8e2c7affd7018d36189f56cb
pratyaksh <pratyaksh.mathur28@gmail.com>
2021-03-31 20:03:34 +0530

    contact email fix

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 const async = require("async");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
+const user = require('../models/user');
 const gPass=process.env.GMAILPW
 
 router.get('/register', (req, res) => {
@@ -166,11 +167,42 @@ router.post('/forgot', function(req, res, next) {
       res.redirect('/home');
     });
   });
+const smtpTransport = nodemailer.createTransport({
+  service: 'gmail', 
+  auth: {
+    user: 'donation.nation.21@gmail.com',
+    pass: gPass
+  }
+})
 
-
-
-
-
+router.post('/send_email', (req, res) => {
+  
+  mail = {
+    to: 'donation.nation.21@gmail.com',
+    subject: 'Contact from '+ req.body.name,
+    body: req.body.message
+  }
+  Sendemail_Contact(mail)
+  res.redirect('/')
+  
+  function Sendemail_Contact(mail) {
+    var mailOptions1 = {
+      to: 'donation.nation.21@gmail.com',
+      from: req.body.email_contact,
+      subject: mail.subject,
+      text: mail.body
+    }
+    smtpTransport.sendMail(mailOptions1, function(err, info) {
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log('Email sent')
+      }
+      
+    })
+  }
+})
 
 
 
